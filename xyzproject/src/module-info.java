c3c1344fba076195e808100006b6eb05f924c38d
shruti2025 <109744631+shruti2025@users.noreply.github.com>
2023-10-26 20:46:31 +0530

    The first commit by 20MIC0059

---
+++ xyzproject/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module xyzproject {
+}
---
+++ xyzproject/src/xyzproject/testTL.java
@@ -0,0 +1,8 @@
+package xyzproject;
+
+public class testTL {
+	public static void main(String args[])
+	{
+		System.out.println("Hello from 20MIC0059");
+	}
+}
